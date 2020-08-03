0abfa7bd05482f1ae7b505a879701297aa2de3d2
Toni Claußner <claussner.toni@gmail.com>
2020-08-03 10:45:59 +0200

    Projektdateien hinzufügen.

---
+++ PeanutExplodes/Eventhandler.cs
@@ -0,0 +1,35 @@
+﻿using Synapse.Events.Classes;
+using Grenades;
+using UnityEngine;
+using Mirror;
+using System;
+using System.Linq;
+
+namespace PeanutExplodes
+{
+    public class Eventhandler
+    {
+        public void OnPlayerHurt(PlayerHurtEvent ev)
+        {
+            if (ev.Player.Role == RoleType.Scp173 && ev.Player.Health - ev.Amount <= 0)
+            {
+                try
+                {
+                    for (int i = 0; i < PeanutExplodes.magnitude; i++)
+                    {
+                        var gm = ev.Player.GetComponent<GrenadeManager>();
+                        var grenade = gm.availableGrenades.FirstOrDefault(g => g.inventoryID == ItemType.GrenadeFrag);
+                        var component = UnityEngine.Object.Instantiate(grenade.grenadeInstance).GetComponent<Grenade>();
+                        component.InitData(gm, Vector3.zero, Vector3.zero, 0f);
+                        NetworkServer.Spawn(component.gameObject);
+                        component.fuseTime = 0;
+                    }
+                }
+                catch(Exception e)
+                {
+                    Synapse.Log.Error(e.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ PeanutExplodes/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using Synapse.Api.Plugin;
+using Synapse;
+using Synapse.Events;
+
+namespace PeanutExplodes
+{
+    [PluginDetails(
+        Author = "BlackApp",
+        Description = "Peanut (SCP-173) explodes when killed",
+        Name = "PeanutExplodes",
+        SynapseMajor = 1,
+        SynapseMinor = 0,
+        SynapsePatch = 0,
+        Version = "1.0.0"
+        )]
+    public class PeanutExplodes : Plugin
+    {
+        private Eventhandler Eventhandler;
+
+        public static int magnitude;
+
+        public override void OnEnable()
+        {
+            Log.Info($"PeanutExplodes successful. Created by {Details.Author}");
+            magnitude = Config.GetInt("PeanutExplodes_magnitude", 1);
+            Eventhandler = new Eventhandler();
+            Events.PlayerHurtEvent += Eventhandler.OnPlayerHurt;
+        }
+    }
+}
