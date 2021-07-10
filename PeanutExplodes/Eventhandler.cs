6efd59720b71abae7b32a1cc6a75cfcc0709aece
Toni Claußner <claussner.toni@gmail.com>
2021-07-10 18:54:45 +0200

    Projektdateien hinzufügen.

---
+++ PeanutExplodes/Eventhandler.cs
@@ -0,0 +1,28 @@
+﻿using Synapse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeanutExplodes
+{
+    public class Eventhandler
+    {
+        public Eventhandler()
+        {
+            Server.Get.Events.Player.PlayerDeathEvent += OnDeath;
+        }
+
+        public void OnDeath(Synapse.Api.Events.SynapseEventArguments.PlayerDeathEventArgs ev)
+        {
+            try
+            {
+                if (ev.Victim.RoleType == RoleType.Scp173)
+                {
+                    Synapse.Api.Map.Get.Explode(ev.Victim.Position, Synapse.Api.Enum.GrenadeType.Grenade, ev.Victim);
+                }
+            } catch (Exception e){}
+        }
+    }
+}
---
+++ PeanutExplodes/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using Synapse.Api.Plugin;
+using Synapse;
+using Synapse.Events;
+using Synapse.Api.Events;
+using GameCore;
+using Synapse.Api;
+
+namespace PeanutExplodes
+{
+    [PluginInformation(
+        Author = "BlackApp",
+        Description = "Peanut (SCP-173) explodes when killed",
+        LoadPriority = 0,
+        Name = "PeanutExplodes",
+        SynapseMajor = 2,
+        SynapseMinor = 6,
+        SynapsePatch = 1,
+        Version = "v1.0.1"
+        )]
+
+    public class PluginClass : AbstractPlugin
+    {
+
+        public override void Load()
+        {
+            Logger.Get.Info("PeanutExplodes successfully loaded!");
+
+            new Eventhandler();
+        }
+    }
+}
